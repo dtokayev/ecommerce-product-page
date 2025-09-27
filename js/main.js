6e8073e4d8343d1cd21e41646484fa4de272c868
Dmytro Tokayev <60584648+dtokayev@users.noreply.github.com>
2025-09-27 16:24:59 +0200

    add desktop gallery controls

--- js/main.js
+++ js/main.js
@@ -24,3 +24,16 @@ increaseAmountButton.addEventListener('click', e => {
   const currentAmount = Number(currentAmountText.textContent);
   currentAmountText.textContent = currentAmount + 1;
 });
+
+let currentImg = 0;
+const thumbnails = document.querySelectorAll('.product__gallery__thumbnails button');
+const images = document.querySelectorAll('.product__gallery__img li');
+for (let i = 0; i < thumbnails.length; i++) {
+  thumbnails[i].addEventListener('click', e => {
+    thumbnails[currentImg].classList.remove('selected-thumbnail');
+    images[currentImg].classList.remove('active-img');
+    currentImg = i;
+    thumbnails[currentImg].classList.add('selected-thumbnail');
+    images[currentImg].classList.add('active-img');
+  });
+}
