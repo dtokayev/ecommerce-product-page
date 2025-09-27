2dea2ebedac22dededf625894cf62759003bd745
Dmytro Tokayev <60584648+dtokayev@users.noreply.github.com>
2025-09-27 13:33:33 +0200

    implement item amount selector

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ navToggle.addEventListener('click', e => {
     navToggle.setAttribute('aria-expanded', 'false');
   }
 });
+
+const currentAmountText = document.querySelector('.product__selected-amount');
+const decreaseAmountButton = document.querySelector('.decrease-amount-btn');
+const increaseAmountButton = document.querySelector('.increase-amount-btn');
+
+decreaseAmountButton.addEventListener('click', e => {
+  const currentAmount = Number(currentAmountText.textContent);
+  if (currentAmount > 0) {
+    currentAmountText.textContent = currentAmount - 1;
+  }
+});
+
+increaseAmountButton.addEventListener('click', e => {
+  const currentAmount = Number(currentAmountText.textContent);
+  currentAmountText.textContent = currentAmount + 1;
+});
